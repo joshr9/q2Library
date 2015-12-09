e3f7f0c8cb4ebd647355c9d1b4c6207cb8f43f63
joshr9 <joshruchwarger@gmail.com>
2015-12-09 09:59:55 -0700

    understanding npm

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ var app = express();
 var books = ["Javascript-The Good Parts", "Eloquent Javascript",
 "Horton Hears a Who"];
 
-app.get('/cowsay/:cowsays', function(request, response){
-  response.setHeader('content-type','text/html');
-  response.send(
-    "<pre>" +
-      cowsay.say({'text': request.params.cowsays}) +
-    "</pre>"
-  );
-})
+// app.get('/cowsay/:cowsays', function(request, response){
+//   response.setHeader('content-type','text/html');
+//   response.send(
+//     "<pre>" +
+//       cowsay.say({'text': request.params.cowsays}) +
+//     "</pre>"
+//   );
+// })
 
 app.get('/', function(request, response){
   response.json(books);
 });
 
-app.listen(8080);
+app.listen(8080, function(){
+  console.log('listening on port 8080');
+});
